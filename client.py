f7202922860426561edcae01b35e938c8b7448b2
Daniel Dương <daniel.duong@outlook.com>
2020-06-13 09:47:28 -0300

    Put constants into a file and add socket code
    The constants can then be shared with the server code.

--- client.py
+++ client.py
@@ -1,33 +1,47 @@
+import socket
 import time
+import constants as c
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
 from typing import List, Tuple
 
-HOSTNAME = 'localhost'
-PORT = 1337
-
-ResponseTimes = namedtuple('ResponseTimes', ('short_time', 'long_time'))
+ResponseTimes = namedtuple('ResponseTimes', ('short_time', 'long_time', 'short_succeed', 'long_succeed'))
 Stat = namedtuple('Stat', ('total_time', 'response_times'))
 
 
-def send_short_request():
-    pass
+def send_msg(msg: bytes) -> bool:
+    sock = socket.create_connection((c.HOSTNAME, c.PORT))
+    succeed = True
+    try:
+        sock.send(msg)
+        sock.recv(1)
+    except Exception:
+        succeed = False
+    finally:
+        sock.close()
+    return True
 
 
-def send_long_request():
-    pass
+def send_short_request() -> bool:
+    """Send a short request. Returns True on success, False otherwise"""
+    return send_msg(c.SHORT)
+
+
+def send_long_request() -> bool:
+    """Send a long request. Returns True on success, False otherwise"""
+    return send_msg(c.LONG)
 
 
 def response_times(nb_requests: int, now=time.time_ns) -> List[ResponseTimes]:
     results = []
     for _ in range(nb_requests):
         start = now()
-        send_short_request()
+        short_succeed = send_short_request()
         end_short = now()
         time_short = end_short - start
-        send_long_request()
+        long_succeed = send_long_request()
         time_long = now() - end_short
-        results.append(ResponseTimes(time_short, time_long))
+        results.append(ResponseTimes(time_short, time_long, short_succeed, long_succeed))
     return results
 
 
---
+++ constants.py
@@ -0,0 +1,4 @@
+HOSTNAME = 'localhost'
+PORT = 1337
+SHORT = b'1'
+LONG = b'2'
